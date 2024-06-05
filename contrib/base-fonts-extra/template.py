0d5fea60c5bb22a3b82d79dc908801f91475a3a1
claybie <claybie@claybie.org>
2024-06-04 22:21:45 -0400

    Add base-fonts and base-fonts-extra

---
+++ contrib/base-fonts-extra/template.py
@@ -0,0 +1,25 @@
+pkgname = "base-fonts-extra"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    "fonts-firacode-ttf",
+    "fonts-font-awesome-otf",
+    "fonts-freefont-otf",
+    "fonts-hack-ttf",
+    "fonts-libertinus-otf",
+    "fonts-nerd-fonts",
+    "fonts-noto",
+    "fonts-noto-emoji-ttf",
+    "fonts-noto-sans-cjk",
+    "fonts-noto-serif-cjk",
+    "fonts-opensans-ttf",
+    "fonts-roboto-ttf",
+    "fonts-tex-gyre-otf",
+    "fonts-twemoji-ttf",
+    "fonts-xorg",
+]
+pkgdesc = "All available fonts in the Chimera contrib repository"
+maintainer = "claybie <claybie@claybie.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
---
+++ main/base-fonts/template.py
@@ -0,0 +1,15 @@
+pkgname = "base-fonts"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    "fonts-cantarell-otf",
+    "fonts-dejavu",
+    "fonts-liberation",
+    "fonts-source-code-pro-otf",
+    "fonts-source-sans-otf",
+]
+pkgdesc = "All available fonts in the Chimera main repository"
+maintainer = "claybie <claybie@claybie.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
